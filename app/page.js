a8f901534083735471fa04bfc81faa7012ac9266
Md-Redwan <redwanhosen4@gamil.com>
2025-11-16 01:36:16 +0600

    Your Protal section is added

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import HeroSection from "./component/HeroSection";
 import NightMares from "./component/NightMares";
 import Greetings from "./component/Greetings";
 import NeonSectorOverview from "./component/NeonSectorOverview";
+import YourProtal from "./component/YourProtal";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <NightMares />
         <Greetings />
         <NeonSectorOverview />
+        <YourProtal />
       </div>
     </main>
   );
